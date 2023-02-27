3e4d59c3b5ef31461d36b6f433752478781f590b
Pere1J <pere1josep@gmail.com>
2023-02-27 12:43:47 +0100

    actualized project java

---
+++ JavaOpenBproject01/src/com/company/Tipos.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Tipos {
+    public static void main(String[] args) {
+        //enteros
+        byte number1 = 1;
+        short num2 = 1; // 2byte
+        // 4 y 8 byte
+        int num3 = 3;
+        long num4 = 4;
+
+        //decimales
+        float decimal = 1.1F;
+        double num1 = 1.99;
+
+        //caracter
+        char caracter1 = 'a';
+
+        boolean verdadero = true;
+
+        //hasta aquí son datos primitivos
+
+
+
+
+    }
+}
