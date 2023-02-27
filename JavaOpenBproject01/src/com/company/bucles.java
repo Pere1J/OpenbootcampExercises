baabd81b96ef6e86e14dac4924e7fa5d83769a72
Pere1J <pere1josep@gmail.com>
2023-02-27 15:57:09 +0100

    understanding while loop

---
+++ JavaOpenBproject01/src/com/company/bucles.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class bucles {
+
+    public static void main(String[] args) {
+
+        int count = 0;
+        while (count<10) {
+
+            System.out.println("contador = " + count);
+            count++;// esto indica que count debe incrementarse
+        }
+        System.out.println("fin");// esto está fuera del while para que se imprima al finalizar
+    }
+}
