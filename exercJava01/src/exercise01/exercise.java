605b0354587656241f85e823d1d9bedb495b0ecf
Pere1J <pere1josep@gmail.com>
2023-02-27 15:11:46 +0100

    added exrcise Java 01

---
+++ exercJava01/src/exercise01/exercise.java
@@ -0,0 +1,31 @@
+package exercise01;
+
+public class exercise {
+    public static void main(String[] args) {
+        //tipo numérico
+        int number = 3;
+        long numberLong = 20000;
+        float decimalNum = 0.1f;
+        double decimNum = 0.01;
+
+        // tipo booleanos
+        boolean verdadero = true;
+        boolean falso = false;
+
+        //tipo texto
+        char letra = 'a';
+        String texto = "esto es una cadena de texto";
+
+        System.out.println(number);
+        System.out.println(numberLong);
+        System.out.println(decimalNum);
+        System.out.println(decimNum);
+        System.out.println(verdadero);
+        System.out.println(falso);
+        System.out.println(letra);
+        System.out.println(texto);
+
+
+
+    }
+}
