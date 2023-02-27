ad465d64e213e8429b14f9223e93bd168635a346
Pere1J <pere1josep@gmail.com>
2023-02-27 14:31:04 +0100

    testing functions in Java

---
+++ JavaOpenBproject01/src/com/company/Funciones.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Funciones {
+
+    public static void main(String[] args) {
+
+        imprimeNombre("Pepe");
+
+    }
+
+    public static void imprimeNombre(String name) {
+        System.out.println("hola " + name);
+
+    }
+}
--- JavaOpenBproject01/src/com/company/Main.java
+++ JavaOpenBproject01/src/com/company/Main.java
@@ -1,4 +1,15 @@
 package com.company;
 
 public class Main {
+    public static void main(String[] args) {
+    // llamamos a una función
+
+        holaMundo();}
+
+        private static void holaMundo() {
+
+        System.out.println("hola mundo");
+        System.out.println("Hola mundo");
+
+    };
 }
